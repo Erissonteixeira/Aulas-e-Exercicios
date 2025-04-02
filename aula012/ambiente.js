8b014fdc2b2229f2d7a9c7d8725fb78245b577ed
Erisson Teixeira <erisson.trabalhos@gmail.com>
2025-04-02 11:07:23 -0300

    subindo aula 12

---
+++ aula012/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+do  {
+console.log(`passo ${c}`)
+c++
+} while (c <=6)
+
+
+
+
+
+/*var c = 1
+while (c <= 12) {
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+
+
+
+/*
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+*/
---
+++ aula012/node_8849f97c51841.tmp.js
@@ -0,0 +1,26 @@
+var c = 1
+while(`passo ${c}`) {
+    console.log('Olá tudo bem?')
+    c++
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+console.log('Olá tudo bem?')
+*/
